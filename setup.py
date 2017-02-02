169481a2ac125a402c64d99d8c005165b7066dca
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2017-02-02 10:50:28 +0200

    Upgraded to Django 1.10.5 and Wagail 1.6.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_search',
-    version='1.0.2',
+    version='2.0.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Search',
